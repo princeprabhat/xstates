d19cc98d28e2b7e7b0aeb27c5ec5e9b3d6d801f7
princeprabhat <princeprabhat1996@gmail.com>
2025-02-07 00:52:26 +0530

    Fixed:Display Bar

--- src/components/MessageBanner.jsx
+++ src/components/MessageBanner.jsx
@@ -6,10 +6,9 @@ const MessageBanner = ({ country, state, city }) => {
       {country && state && city && (
         <div style={{ fontSize: "18px", fontWeight: "900" }}>
           You selected{" "}
-          <span style={{ fontWeight: "900", fontSize: "25px" }}>{country}</span>
-          ,{" "}
+          <span style={{ fontWeight: "900", fontSize: "25px" }}>{city}</span>,{" "}
           <span style={{ color: "gray" }}>
-            {state}, {city}
+            {state}, {country}
           </span>
         </div>
       )}
